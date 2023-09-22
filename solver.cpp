ce84521305822e10c505e91c320f400e5705445c
Benjamin Hennies <benjaminhennies@posteo.de>
2023-09-22 10:41:32 +0200

    disable statistics

--- solver.cpp
+++ solver.cpp
@@ -12,7 +12,9 @@
 #include "clause.h"
 #include "encoding_util.h"
 
+#if COLLECT_SOLVER_STATISTICS
 Solver_Stats solverStats;
+#endif
 
 bool Solver::addClause(const std::vector<Literal_t> &literals, bool learnt) {
     if (literals.empty()) {
